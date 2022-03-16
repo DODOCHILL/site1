f775fba1c2fc610ce90dc9dfc788b6788480fc78
DODOCHILL <101755848+DODOCHILL@users.noreply.github.com>
2022-03-17 00:40:13 +0200

    MartaUp

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header__slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header__dots',
+    });
+});
